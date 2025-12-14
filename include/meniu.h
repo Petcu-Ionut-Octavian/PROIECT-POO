713c9e63db47739b027f8b9eb020c662eea2b918
Petcu-Ionut-Octavian <ionutoctavianpetcu@gmail.com>
2025-12-15 00:30:36 +0200

    Update meniu.h

--- include/meniu.h
+++ include/meniu.h
@@ -12,7 +12,6 @@
 class FastFoodMenu {
 private:
     static FastFoodMenu* instance;
-    std::vector<std::string> items;
 
     FastFoodMenu() = default;
 
@@ -24,7 +23,6 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const FastFoodMenu& menu);
     friend std::istream& operator>>(std::istream& is, FastFoodMenu& menu);
 
-    void addItem(const std::string& item);
     void showMenu() const;
 };
 
@@ -71,33 +69,123 @@ public:
 class Produs {
 protected:
     std::string nume;
-    double pret;
+    double pret_baza;
+    int cantitate; // grame
 
 public:
-    Produs(const std::string& nume, double pret) : nume(nume), pret(pret) {}
+    Produs(const std::string& nume, double pret_baza) : nume(nume), pret_baza(pret_baza) {}
     virtual ~Produs() = default;
 
     virtual void afiseaza() const = 0; // metodă abstractă
-    double getPret() const { return pret; }
-    std::string getNume() const { return nume; }
+
+    // Getteri
+    double getPretBaza() const;
+    std::string getNume() const;
+    int getCantitate() const;
+
+    // Setteri
+    void setPretBaza(double pret);
+    void setNume(const std::string& numeNou);
+    void setCantitate(int grame);
 };
 
-class Cartofi : public Produs {
+class Cartofi final : public Produs {
+private:
+    bool are_ketchup;
+    bool are_maioneza;
+    bool are_mustar;
+    bool are_sos_picant;
+
 public:
-    Cartofi(double pret) : Produs("Cartofi", pret) {}
+    Cartofi(double pret_baza) : Produs("Cartofi", pret_baza) {}
+
     void afiseaza() const override;
+
+    // Getteri
+    bool getAreKetchup() const;
+    bool getAreMaioneza() const;
+    bool getAreMustar() const;
+    bool getAreSosPicant() const;
+
+    // Setteri
+    void setAreKetchup(bool val);
+    void setAreMaioneza(bool val);
+    void setAreMustar(bool val);
+    void setAreSosPicant(bool val);
 };
 
-class Pizza : public Produs {
+class Pizza final : public Produs {
+private:
+    bool are_salam;
+    bool are_ciuperci;
+    bool are_masline;
+    bool are_extra_cheese;
+    bool are_ketchup;
+    bool are_usturoi;
+
 public:
-    Pizza(double pret) : Produs("Pizza", pret) {}
+    Pizza(double pret_baza) : Produs("Pizza", pret_baza) {}
+
     void afiseaza() const override;
+
+    // Getteri
+    bool getAreSalam() const;
+    bool getAreCiuperci() const;
+    bool getAreMasline() const;
+    bool getAreExtraCheese() const;
+    bool getAreKetchup() const;
+    bool getAreUsturoi() const;
+
+    // Setteri
+    void setAreSalam(bool val);
+    void setAreCiuperci(bool val);
+    void setAreMasline(bool val);
+    void setAreExtraCheese(bool val);
+    void setAreKetchup(bool val);
+    void setAreUsturoi(bool val);
 };
 
-class Shaorma : public Produs {
+class Shaorma final : public Produs {
+private:
+    bool are_pui;
+    bool are_vita;
+    bool are_porc;
+    bool are_cartofi;
+    bool are_salata;
+    bool are_varza;
+    bool are_rosii;
+    bool are_castraveti;
+    bool are_maioneza;
+    bool are_ketchup;
+
 public:
-    Shaorma(double pret) : Produs("Shaorma", pret) {}
+    Shaorma(double pret_baza) : Produs("Shaorma", pret_baza) {}
+
     void afiseaza() const override;
+
+    // Getteri
+    bool getArePui() const;
+    bool getAreVita() const;
+    bool getArePorc() const;
+    bool getAreCartofi() const;
+    bool getAreSalata() const;
+    bool getAreVarza() const;
+    bool getAreRosii() const;
+    bool getAreCastraveti() const;
+    bool getAreMaioneza() const;
+    bool getAreKetchup() const;
+
+    // Setteri
+    void setArePui(bool val);
+    void setAreVita(bool val);
+    void setArePorc(bool val);
+    void setAreCartofi(bool val);
+    void setAreSalata(bool val);
+    void setAreVarza(bool val);
+    void setAreRosii(bool val);
+    void setAreCastraveti(bool val);
+    void setAreMaioneza(bool val);
+    void setAreKetchup(bool val);
 };
 
 // =======================
